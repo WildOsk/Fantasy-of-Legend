49c9f581584ac235b6c6bf8ef5d455337cb7b4c7
alexmartinez9 <alex.martinez.25.12@gmail.com>
2020-04-27 14:08:45 +0200

    no message

--- FantasyOfLegend/src/main/java/com/Petin/FantasyOfLegend/FantasyOfLegendApplication.java
+++ FantasyOfLegend/src/main/java/com/Petin/FantasyOfLegend/FantasyOfLegendApplication.java
@@ -2,12 +2,20 @@ package com.Petin.FantasyOfLegend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class FantasyOfLegendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FantasyOfLegendApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- FantasyOfLegend/src/main/java/com/Petin/FantasyOfLegend/Rest/UsuarioImpl.java
+++ FantasyOfLegend/src/main/java/com/Petin/FantasyOfLegend/Rest/UsuarioImpl.java
@@ -1,20 +1,16 @@
 package com.Petin.FantasyOfLegend.Rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 
-import org.hibernate.*;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,22 +22,60 @@ import com.Petin.FantasyOfLegend.Entity.Usuario;
 
 @org.springframework.web.bind.annotation.RestController
 @CrossOrigin(origins = "*", methods= {RequestMethod.GET, RequestMethod.POST})
-@RequestMapping("/usuarios")
 public class UsuarioImpl {
 	
 	@PersistenceContext
 	private EntityManager entityManager;
 	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	@Autowired
+	private UsuarioDao usu;
+	
 	@Transactional
 	@PostMapping
-	public void insert(@RequestBody Usuario usuario){
-		entityManager.createNativeQuery("INSERT INTO usuario (nombre,apellido,correo,alias,contrasena) VALUES (?,?,?,?,?)")
-			.setParameter(1, usuario.getNombre())
-			.setParameter(2, usuario.getApellido())
-			.setParameter(3, usuario.getCorreo())
-			.setParameter(4, usuario.getAlias())
-			.setParameter(5, usuario.getContrasena())
-			.executeUpdate();
-		entityManager.close();
+	@RequestMapping("/usuarios")
+	public boolean insert(@RequestBody Usuario usuario){
+		boolean existe = false;
+		Query query = entityManager.createNativeQuery("Select id from usuario where alias= ?")
+				.setParameter(1, usuario.getAlias());
+		List<Integer> resultList = query.getResultList();
+		if(resultList.size()>0) {
+			return false;
+		} else {
+			query = entityManager.createNativeQuery("Select id from usuario where correo= ?")
+					.setParameter(1, usuario.getCorreo());
+			resultList = query.getResultList();
+			if(resultList.size()>0) {
+				return false;
+			} else {
+				entityManager.createNativeQuery("INSERT INTO usuario (nombre,apellido,correo,alias,contrasena) VALUES (?,?,?,?,?)")
+				.setParameter(1, usuario.getNombre())
+				.setParameter(2, usuario.getApellido())
+				.setParameter(3, usuario.getCorreo())
+				.setParameter(4, usuario.getAlias())
+				.setParameter(5, bCryptPasswordEncoder.encode(usuario.getContrasena()))
+				.executeUpdate();
+			entityManager.close();
+			}
+			return true;
+		}
+	}
+	
+	@PostMapping
+	@RequestMapping("/usuarios/login")
+	public Optional<Usuario> iniciarSesion(@RequestBody Usuario usuario) {
+		System.out.println(bCryptPasswordEncoder.encode(usuario.getContrasena()));
+		Query query = entityManager.createNativeQuery("Select id from usuario where alias= ? and contrasena = ?")
+				.setParameter(1, usuario.getAlias())
+				.setParameter(2, bCryptPasswordEncoder.encode(usuario.getContrasena()));
+		int result = -1;
+		try {
+			result = (int) query.getSingleResult();
+			entityManager.close();		
+			return usu.findById(result);
+		} catch (Exception e) {
+			return null;
+		}
 	}
 }
