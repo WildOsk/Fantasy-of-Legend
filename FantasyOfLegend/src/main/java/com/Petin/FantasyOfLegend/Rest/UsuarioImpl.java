49a7066d1b67e9d3af95b45c94c67d371170276f
alexmartinez9 <alex.martinez.25.12@gmail.com>
2020-04-29 17:11:57 +0200

    no message

--- FantasyOfLegend/src/main/java/com/Petin/FantasyOfLegend/Rest/UsuarioImpl.java
+++ FantasyOfLegend/src/main/java/com/Petin/FantasyOfLegend/Rest/UsuarioImpl.java
@@ -7,6 +7,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,12 +66,40 @@ public class UsuarioImpl {
 		Query query = entityManager.createNativeQuery("Select id from usuario where alias= ? and contrasena = hex(AES_ENCRYPT(?,'PATATON'))")
 				.setParameter(1, usuario.getAlias())
 				.setParameter(2, usuario.getContrasena());
-		int result = -1;
+		
 		try {
-			result = (int) query.getSingleResult();
+			int result = (int) query.getSingleResult();
 			entityManager.close();		
 			return usu.findById(result);
 		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	@Transactional(propagation = Propagation.NESTED)
+	@PostMapping
+	@RequestMapping("/usuarios/edit")
+	public Optional<Usuario> edit(@RequestBody Usuario usuario) {
+		System.out.println(usuario.toString());
+		Query query = entityManager.createNativeQuery("Select id from usuario where alias= ?")
+				.setParameter(1, usuario.getAlias());
+		List<Integer> resultList = query.getResultList();
+		if(resultList.size()>0) {
+			int result =(int) query.getSingleResult();
+			try {
+				entityManager.createNativeQuery("Update Usuario set nombre=?,apellido=?,correo=?,contrasena=hex(AES_ENCRYPT(?,'PATATON')) where id= ?")
+						.setParameter(1, usuario.getNombre())
+						.setParameter(2, usuario.getApellido())
+						.setParameter(3, usuario.getCorreo())
+						.setParameter(4, usuario.getContrasena())
+						.setParameter(5, result)
+						.executeUpdate();
+				entityManager.close();
+				return usu.findById(result);
+			} catch (Exception e) {
+				return null;
+			}
+		}else {
 			return null;
 		}
 	}
