7b750b7fef7d1f1f225a9c32c1a9f711b47b7eb2
alexmartinez9 <alex.martinez.25.12@gmail.com>
2020-06-05 17:01:35 +0200

    no message

--- FantasyOfLegend/src/test/java/com/Petin/FantasyOfLegend/FantasyOfLegendApplicationTests.java
+++ FantasyOfLegend/src/test/java/com/Petin/FantasyOfLegend/FantasyOfLegendApplicationTests.java
@@ -1,13 +1,24 @@
 package com.Petin.FantasyOfLegend;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import javax.validation.constraints.AssertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.Petin.FantasyOfLegend.Rest.LigaImpl;
 
 @SpringBootTest
 class FantasyOfLegendApplicationTests {
 
 	@Test
 	void contextLoads() {
+		LigaImpl liga = new LigaImpl();
+		
+		String r = liga.codigoRandom();
+		
+		assertTrue(r!=null);
 	}
 
 }
